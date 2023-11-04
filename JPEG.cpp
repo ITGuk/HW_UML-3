132a49897654d3c050cf5fcbf3573f5b1b9d0458
Andrii <128259268+Pasteras@users.noreply.github.com>
2023-11-04 16:37:01 +0200

    Add files via upload

---
+++ JPEG.cpp
@@ -0,0 +1,11 @@
+#include "JPEG.h"
+
+void JPEG::load()
+{
+	cout << "Loading JPEG image...\n";
+}
+
+void JPEG::save()
+{
+	cout << "Save JPEG image.\n";
+}
---
+++ PNG.h
@@ -0,0 +1,11 @@
+#pragma once
+#include "ImageProcessor.h"
+class PNG:
+    public ImageProcessor
+{
+public:
+    void load() override;
+
+    void save() override;
+};
+
